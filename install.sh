8fe2f7468e4ea9a08a55b2d9b7288348a8ebbfd3
ZansLord <90433321+ZansLord@users.noreply.github.com>
2021-11-27 15:38:42 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update && apt upgrade
+apt-get install nodejs
+nodejs -v
+apt-get install npm
+apt install ffmpeg imagemagick
+apt install ruby
+apt install git
+npm install
+npm update
+npm audit fix --force
+echo "install bahan bahan done, silahkan ketik npm start untuk memulai bot"
